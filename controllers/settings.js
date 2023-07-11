1e4068e043e566da87b1beb537ff9ce9f91e193b
Amirul987 <amirulfahrudin750@gmail.com>
2023-07-11 08:23:46 +0700

    gordon

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "NobitaOfc";
+global.key_free = ["freenobita"];
+global.key_premium = [];
+global.limit_free = "50";
+global.limit_premium = "99999";
+global.dateLimit = "February, 2 2022",
+global.mongo_Db = "mongodb+srv://skiebkdob:lupasandi123@cluster0.bvmjmvc.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "akunepep94746@gmail.com";
+global.my_email_password = "fcrzylihgvfvwkkp";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
